f8efda0a2a3767645c86cc2945bf56a8e8cd90ec
IvanChumakov <36165391+IvanChumakov@users.noreply.github.com>
2019-10-12 10:13:46 +0300

    Add files via upload

---
+++ turtle1Task.py
@@ -0,0 +1,12 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:13:57) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> t = turtle.Pen()
+>>> for a in range(2):
+	t.forward(70)
+	t.left(90)
+	t.forward(40)
+	t.left(90)
+
+	
+>>> 
